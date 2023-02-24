6f7631a268dccfa2f714de8cae65435b03560e1e
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2023-02-25 03:53:17 +0700

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const verifyToken = require("../middlewares/verifyToken");
+
+const usersHandler = require("./handler/users");
+
+/* POST users. */
+router.post("/register", usersHandler.register);
+router.post("/login", usersHandler.login);
+router.post("/logout", verifyToken, usersHandler.logout);
+router.put("/", verifyToken, usersHandler.update);
+router.get("/", verifyToken, usersHandler.getUser);
+
+module.exports = router;
